51c7ac0ad402a068030a5c7aea0b0602d5c533a5
pPrecel <filip.strozik@outlook.com>
2020-12-11 23:53:38 +0100

    Add search icon

--- scripts/natural.sh
+++ scripts/natural.sh
@@ -4,13 +4,17 @@ current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$current_dir/os-helpers.sh"
 
 main() {
+    local clock_icon=""
+    local calendar_icon=""
+    local search_icon=""
+    #local bell_icon=""
     os-helpers::set_os_icon
 
     tmux set-option -g status-left " ${os_icon}  #[fg=default]|"
-    tmux set-option -g status-right "|  %H:%M  %d-%b-%y"
-    tmux set-option -gw window-status-separator "|"
-    tmux set-option -gw window-status-format " #I:#W "
-    tmux set-option -gw window-status-current-format "#[fg=yellow, bold] #I:#W "
+    tmux set-option -g status-right "| $clock_icon %H:%M $calendar_icon %d-%b-%y"
+    tmux set-option -gw window-status-separator " | "
+    tmux set-option -gw window-status-format "#{?window_zoomed_flag,$search_icon ,} #I:#W"
+    tmux set-option -gw window-status-current-format "#[fg=yellow, bold]#{?window_zoomed_flag,$search_icon ,} #I:#W"
 }
 
 main
